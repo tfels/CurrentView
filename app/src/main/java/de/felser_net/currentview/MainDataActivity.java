5d43b719bc1cbe327a22eea48caf533af7959134
Tim Felser <tim@felser-net.de>
2016-04-30 20:55:27 +0200

    initial check-in

---
+++ app/src/main/java/de/felser_net/currentview/MainDataActivity.java
@@ -0,0 +1,13 @@
+package de.felser_net.currentview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_data);
+    }
+}
