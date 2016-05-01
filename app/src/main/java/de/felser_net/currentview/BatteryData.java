c52d623535813279c2ed709eea273c56e27a321f
Tim Felser <tim@felser-net.de>
2016-05-01 11:24:26 +0200

    showing just some battery data in main activity

---
+++ app/src/main/java/de/felser_net/currentview/BatteryData.java
@@ -0,0 +1,120 @@
+package de.felser_net.currentview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+
+/**
+ * Created by tf on 30.04.2016.
+ */
+public class BatteryData {
+
+    private Context context = null;
+
+    private int status = BatteryManager.BATTERY_STATUS_UNKNOWN;
+    private int health = 0;
+    private boolean present = false;
+    private int level = -1;
+    private int scale = -1;
+    private int icon = 0;
+    private int plugged = 0;
+    private int voltage = 0;
+    private int temperature = 0;
+    private String technology = "";
+    private int invalid_charger = 0;
+
+    public BatteryData(Context iContext) {
+        context = iContext;
+    }
+
+    public void updateData() {
+        Intent batteryStatus = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        health = batteryStatus.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
+        present = batteryStatus.getBooleanExtra(BatteryManager.EXTRA_PRESENT, false);
+        level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        icon = batteryStatus.getIntExtra(BatteryManager.EXTRA_ICON_SMALL, -1);
+        plugged = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        voltage = batteryStatus.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+        temperature = batteryStatus.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+        technology = batteryStatus.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY);
+        //invalid_charger = batteryStatus.getIntExtra(BatteryManager.EXTRA_INVALID_CHARGER, -1);
+    }
+
+    public String getStatusText() {
+        switch(status) {
+            case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                return context.getResources().getString(R.string.valUnknown);
+            case BatteryManager.BATTERY_STATUS_CHARGING:
+                return context.getResources().getString(R.string.valCharging);
+            case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                return context.getResources().getString(R.string.valDischarging);
+            case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                return context.getResources().getString(R.string.valNotCharging);
+            case BatteryManager.BATTERY_STATUS_FULL:
+                return context.getResources().getString(R.string.valFull);
+            default:
+                return context.getResources().getString(R.string.valUnknownValue);
+        }
+    }
+
+    public String getHealthText() {
+        switch(health) {
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                return context.getResources().getString(R.string.valUnknown);
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                return context.getResources().getString(R.string.valGood);
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                return context.getResources().getString(R.string.valOverHeat);
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                return context.getResources().getString(R.string.valDead);
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                return context.getResources().getString(R.string.valOverVoltage);
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                return context.getResources().getString(R.string.valUnspecifiedFailure);
+            case BatteryManager.BATTERY_HEALTH_COLD:
+                return context.getResources().getString(R.string.valCold);
+            default:
+                return context.getResources().getString(R.string.valUnknownValue);
+        }
+    }
+
+    public String getPresentText() {
+        return new Boolean(present).toString();
+    }
+    public String getLevelText() {
+        return new Integer(level).toString() + "%";
+    }
+    public String getScaleText() {
+        return new Integer(scale).toString() + "%";
+    }
+    public int getIconId() {
+        return icon;
+    }
+
+    public String getPluggedText() {
+        switch(plugged) {
+            case BatteryManager.BATTERY_PLUGGED_AC:
+                return context.getResources().getString(R.string.valAC);
+            case BatteryManager.BATTERY_PLUGGED_USB:
+                return context.getResources().getString(R.string.valUSB);
+            case BatteryManager.BATTERY_PLUGGED_WIRELESS:
+                return context.getResources().getString(R.string.valWireless);
+            default:
+                return context.getResources().getString(R.string.valUnknownValue);
+        }
+    }
+
+    public String getVoltageText() {
+        return new Float(voltage/1000.0).toString() + "V";
+    }
+    public String getTemperatureText() {
+        return new Float(temperature/10.0).toString() + "°";
+    }
+    public String getTechnologyText() {
+        return technology;
+    }
+}
--- app/src/main/java/de/felser_net/currentview/MainDataActivity.java
+++ app/src/main/java/de/felser_net/currentview/MainDataActivity.java
@@ -1,13 +1,72 @@
 package de.felser_net.currentview;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainDataActivity extends AppCompatActivity {
+
+    private TextView txtValueStatus = null;
+    private TextView txtValueHealth = null;
+    private TextView txtValuePresent = null;
+    private TextView txtValueLevel = null;
+    private TextView txtValueScale = null;
+    private TextView txtValuePlugged = null;
+    private TextView txtValueVoltage = null;
+    private TextView txtValueTemperature = null;
+    private TextView txtValueTechnology = null;
+    private ImageView imgIcon = null;
+
+    private BatteryData batData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_data);
+
+        txtValueStatus = (TextView)findViewById(R.id.textValueStatus);
+        txtValueHealth = (TextView)findViewById(R.id.textValueHealth);
+        txtValuePresent = (TextView)findViewById(R.id.textValuePresent);
+        txtValueLevel = (TextView)findViewById(R.id.textValueLevel);
+        txtValueScale = (TextView)findViewById(R.id.textValueScale);
+        txtValuePlugged = (TextView)findViewById(R.id.textValuePlugged);
+        txtValueVoltage = (TextView)findViewById(R.id.textValueVoltage);
+        txtValueTemperature = (TextView)findViewById(R.id.textValueTemperature);
+        txtValueTechnology = (TextView)findViewById(R.id.textValueTechnology);
+
+        imgIcon  = (ImageView)findViewById(R.id.imageIcon);
+
+        batData = new BatteryData(getApplicationContext());
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        batData.updateData();
+        refreshUi();
+    }
+
+    private void refreshUi()
+    {
+        txtValueStatus.setText(batData.getStatusText());
+        txtValueHealth.setText(batData.getHealthText());
+        txtValuePresent.setText(batData.getPresentText());
+        txtValueLevel.setText(batData.getLevelText());
+        txtValueScale.setText(batData.getScaleText());
+        //txtValueStatus.setText(batData.getIconStatusText());
+        txtValuePlugged.setText(batData.getPluggedText());
+        txtValueVoltage.setText(batData.getVoltageText());
+        txtValueTemperature.setText(batData.getTemperatureText());
+        txtValueTechnology.setText(batData.getTechnologyText());
+
+        imgIcon.setImageResource(batData.getIconId());
+    }
+
 }
+
+
