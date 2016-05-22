eca4d95ceb6ef3119e51469588b24b80fe1b8121
Tim Felser <tim@felser-net.de>
2016-05-22 15:37:45 +0200

    implemented a much simpler (but not so precise) moving algorithm

--- app/src/main/java/de/felser_net/currentview/OverlayWindowService.java
+++ app/src/main/java/de/felser_net/currentview/OverlayWindowService.java
@@ -1,13 +1,10 @@
 package de.felser_net.currentview;
 
-import android.app.IntentService;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
-import android.net.Uri;
 import android.os.IBinder;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -18,13 +15,16 @@ import android.widget.Toast;
 
 /**
  * Created by tf on 21.05.2016.
+ * This service creates and manages the overlay window.
  */
 public class OverlayWindowService extends Service implements View.OnTouchListener, View.OnClickListener {
 
 
     private WindowManager wm = null;
     private Button overlayedButton = null;
-    private View topLeftView = null;
+    private float lastMoveX;
+    private float lastMoveY;
+    private boolean lastMoveValuesValid = false;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -41,7 +41,7 @@ public class OverlayWindowService extends Service implements View.OnTouchListene
         overlayedButton = new Button(this);
         overlayedButton.setText("Overlay button");
         overlayedButton.setOnTouchListener(this);
-        overlayedButton.setAlpha(0.5f);
+        overlayedButton.setAlpha(0.9f);
         overlayedButton.setBackgroundColor(0x55fe4444);
         overlayedButton.setOnClickListener(this);
 
@@ -55,91 +55,53 @@ public class OverlayWindowService extends Service implements View.OnTouchListene
         params.x = 0;
         params.y = 0;
         wm.addView(overlayedButton, params);
-
-        topLeftView = new View(this);
-        WindowManager.LayoutParams topLeftParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
-        topLeftParams.gravity = Gravity.LEFT | Gravity.TOP;
-        topLeftParams.x = 0;
-        topLeftParams.y = 0;
-        topLeftParams.width = 0;
-        topLeftParams.height = 0;
-        wm.addView(topLeftView, topLeftParams);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.e("TF", "service onDestroy");
-
         if (overlayedButton != null) {
             wm.removeView(overlayedButton);
-            wm.removeView(topLeftView);
             overlayedButton = null;
-            topLeftView = null;
         }
     }
-
-    private boolean moving = false;
-    private int originalXPos;
-    private int originalYPos;
-
-    private float offsetX;
-    private float offsetY;
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            float x = event.getRawX();
-            float y = event.getRawY();
-
-            moving = false;
-
-            int[] location = new int[2];
-            overlayedButton.getLocationOnScreen(location);
-
-            originalXPos = location[0];
-            originalYPos = location[1];
-
-            offsetX = originalXPos - x;
-            offsetY = originalYPos - y;
-
+            lastMoveX = 0;
+            lastMoveY = 0;
+            lastMoveValuesValid = false;
         } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            int[] topLeftLocationOnScreen = new int[2];
-            topLeftView.getLocationOnScreen(topLeftLocationOnScreen);
 
-            System.out.println("topLeftY="+topLeftLocationOnScreen[1]);
-            System.out.println("originalY="+originalYPos);
+            float moveX = event.getRawX();
+            float moveY = event.getRawY();
 
-            float x = event.getRawX();
-            float y = event.getRawY();
+            float offsetX = (moveX - lastMoveX);
+            float offsetY = (moveY - lastMoveY);
 
-            WindowManager.LayoutParams params = (WindowManager.LayoutParams) overlayedButton.getLayoutParams();
+            lastMoveX = moveX;
+            lastMoveY = moveY;
 
-            int newX = (int) (offsetX + x);
-            int newY = (int) (offsetY + y);
-
-            if (Math.abs(newX - originalXPos) < 1 && Math.abs(newY - originalYPos) < 1 && !moving) {
+            if(!lastMoveValuesValid) {
+                lastMoveValuesValid = true;
                 return false;
             }
 
-            params.x = newX - (topLeftLocationOnScreen[0]);
-            params.y = newY - (topLeftLocationOnScreen[1]);
-
+            // update the position
+            WindowManager.LayoutParams params = (WindowManager.LayoutParams) overlayedButton.getLayoutParams();
+            params.x += offsetX;
+            params.y += offsetY;
             wm.updateViewLayout(overlayedButton, params);
-            moving = true;
-        } else if (event.getAction() == MotionEvent.ACTION_UP) {
-            if (moving) {
-                return true;
-            }
-        }
 
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            return lastMoveValuesValid;
+        }
         return false;
     }
 
-
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "Overlay button click event", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Overlay onClick called", Toast.LENGTH_SHORT).show();
     }
 }
